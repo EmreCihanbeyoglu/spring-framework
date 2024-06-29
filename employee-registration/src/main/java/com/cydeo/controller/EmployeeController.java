4e327fa161aaf326a317f2c7c7f5a238e686297f
Emre Cihanbeyoglu <mrcihanbeyoglu@gmail.com>
2024-06-29 21:39:02 +0200

    practice with employee registration form mini-project

--- employee-registration/src/main/java/com/cydeo/controller/EmployeeController.java
+++ employee-registration/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -3,8 +3,10 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.bootstrap.DataStore;
 import com.cydeo.model.Employee;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +27,13 @@ public class EmployeeController {
     }
 
     @PostMapping("/list")
-    public String employeeList(@ModelAttribute("employee") Employee employee, Model model) {
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("stateList", DataGenerator.getStateList());
+            return "/employee/employee-registration";
+        }
+
+
         DataStore.saveEmployeeToStore(employee);
         model.addAttribute("employeeList", DataStore.getEmployeeListFromStore());
         return "/employee/employee-list";
--- employee-registration/src/main/java/com/cydeo/model/Employee.java
+++ employee-registration/src/main/java/com/cydeo/model/Employee.java
@@ -1,16 +1,28 @@
 package com.cydeo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
+
+
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String firstName;
     private String lastName;
-    private String birthDay;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDay;
     private String email;
     private String password;
     private String address;
